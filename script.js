ed1de60e8974614fb929e8459575ea6c838da3d0
joaboch <joakoboch@gmail.com>
2023-11-04 22:36:45 -0300

    Add grid size button

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const grid = document.createElement('div');
 const body = document.querySelector('body');
+const btn = document.createElement('button');
+
+
+
+btn.textContent = 'New grid';
+body.appendChild(btn);
 body.appendChild(grid);
 grid.classList.add('grid');
 
 
 //Create the grid row and insert divs inside it
-for(let i = 0; i < 16; i++) {
-  const gridRow = document.createElement('div');
-  for(let j = 0; j < 16; j++) {
-    const gridSquare = document.createElement('div');
-    gridSquare.classList.add('square');
-    gridRow.appendChild(gridSquare);
+function createGrid(gridSize, squareSize) {
+  for(let i = 0; i < gridSize; i++) {
+    const gridRow = document.createElement('div');
+    for(let j = 0; j < gridSize; j++) {
+      const gridSquare = document.createElement('div');
+      gridSquare.classList.add('square');
+      gridSquare.style.cssText = `width: ${squareSize}; height: ${squareSize}`;
+      gridRow.appendChild(gridSquare);
+    }
+    grid.appendChild(gridRow);
+    gridRow.classList.add('row');
   }
-  grid.appendChild(gridRow);
-  gridRow.classList.add('row');
 }
 
 //Add the hover listener to change color of individual grids
@@ -22,3 +31,11 @@ grid.addEventListener('mouseover', function (e) {
         e.target.classList.add('colorChange');
     }
   });
+
+//Add the button that specifies the grid squares per side
+btn.addEventListener('click', () => {
+  gridSize = prompt('What grid size would you like?');
+  const squareSize = grid.clientWidth / gridSize;
+  console.log(squareSize);
+  createGrid(gridSize, squareSize);
+})
